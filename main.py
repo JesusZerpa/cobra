ada6a470e3a61b85c5c5c9cae1c12b6494875760
jesbram <jesus26abraham1996@gmail.com>
2020-10-02 10:05:30 -0400

    eliminado codigo duplicado

--- main.py
+++ main.py
@@ -9,60 +9,6 @@ frameworks={
 
     "jsee":"https://gitlab.com/jesbram/jsee/-/archive/master/jsee-master.zip"
 }
-exclude=["cobra",
-         "node_modules"
-        ]
-cobrapath=str(pathlib.Path(__file__).parent.absolute())
-def hacer_backup():
-    pass
-def actualizar_framework(path=os.getcwd()):
-    if settings.framework in frameworks:
-        r=requests.get(frameworks[settings.framework],stream=True)
-        with open(cobrapath+"/backups/"+settings.framework+".zip", 'wb') as fd:
-            for chunk in r.iter_content(chunk_size=128):
-                fd.write(chunk)
-        with  ZipFile(cobrapath+"/backups/backup.zip", 'w') as fd:
-            for dirname, subdirs, files in os.walk(path):
-                for filename in files:
-                    filepath=os.path.join(dirname, filename)
-                    
-                    if dirname[len(os.getcwd()+"/"):].split("/")[0] not in exclude:
-       
-                        fd.write(filepath[len(os.getcwd()+"/"):])
-            fd.close()
-
-        
-
-        with ZipFile(cobrapath+"/backups/jsee.zip", 'r') as zipObj:
-            zipObj.extractall(cobrapath+"/backups/")
-
-        for elem in os.listdir(cobrapath+"/backups/"+settings.framework+"-master"):
-            if elem not in ["package.json",
-                            "settings.json",
-                            "index.js",
-                            "package-lock.json",
-                            ]:
-                if os.path.exists(path+"/"+elem): 
-                    if os.path.isfile(path+"/"+elem):
-                        os.remove(path+"/"+elem)
-
-                shutil.move(cobrapath+"/backups/"+settings.framework+"-master/"+elem,path)
-            elif elem=="package.json":
-                with open(path+"/package.json","r") as f:
-       
-                    data2=json.loads(f.read())
-                with open(cobrapath+"/backups/"+settings.framework+"-master/package.json","r") as f:
-                    data=json.loads(f.read())
-                data3=data2
-                for elem2 in data2["dependencies"]: 
-                    for elem in data["dependencies"]:
-                        if elem2==elem:
-                            break
-                    else:
-                        data3["dependencies"][elem2]=data2["dependencies"][elem2]
-                with open(cobrapath+"/backups/"+settings.framework+"-master/package.json","w") as f:
-                    f.write(json.dumps(data3, indent=4, sort_keys=True))
-        print("Sistema actualizado")
 
 exclude=["cobra",
          "node_modules"
@@ -96,6 +42,7 @@ def actualizar_framework(path=os.getcwd()):
                                 "settings.js",
                                 "index.js",
                                 "package-lock.json",
+                                "README.md"
                                 ]:
 
                     if os.path.exists(path+"/"+elem):
